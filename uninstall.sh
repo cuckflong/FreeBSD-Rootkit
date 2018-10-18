b6715147484c0d501596416c3880f007e3d92526
johnathanclong <johnathanliauw@yahoo.com.hk>
2018-10-18 16:59:08 +1100

    changed install

--- uninstall.sh
+++ uninstall.sh
@@ -5,9 +5,7 @@ killall -9 daemon
 rm /sbin/controller
 rm /sbin/priv_esc
 rm /log.txt
-rm /remote.py
 rm /sbin/daemon
 rm /etc/rc.d/login
-echo "" > /boot/loader.conf
 kldunload ./rootkit.ko
 rm /boot/kernel/rootkit.ko
